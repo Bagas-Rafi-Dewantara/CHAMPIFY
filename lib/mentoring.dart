b6cd7b90ee9f045c41d18f1c2476e0393263767f
auliya <aulyamalidi@gmail.com>
2025-11-27 20:40:59 +0700

    homepage

--- lib/mentoring.dart
+++ lib/mentoring.dart
@@ -1,21 +1,5 @@
 import 'package:flutter/material.dart';
 
-void main() {
-  runApp(const MyApp());
-}
-
-class MyApp extends StatelessWidget {
-  const MyApp({Key? key}) : super(key: key);
-
-  @override
-  Widget build(BuildContext context) {
-    return MaterialApp(
-      debugShowCheckedModeBanner: false,
-      home: const ZoomMeetingScreen(),
-    );
-  }
-}
-
 class ZoomMeetingScreen extends StatelessWidget {
   const ZoomMeetingScreen({Key? key}) : super(key: key);
 
