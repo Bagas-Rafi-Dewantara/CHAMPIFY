d9e07a53edff8ddb5250e5db31c376411e89ebbf
Nathania <nathania.kayla18@gmail.com>
2025-11-27 17:03:54 +0700

    login dan sign up

--- lib/main.dart
+++ lib/main.dart
@@ -1,7 +1,10 @@
 import 'package:flutter/material.dart';
-import 'package:google_fonts/google_fonts.dart'; // 1. Jangan lupa import ini
+import 'package:google_fonts/google_fonts.dart';
+
+import 'authentication/signup.dart';
+import 'authentication/login.dart';
+import 'mentoring.dart';
 import 'course/courses.dart';
-import 'course/playlist_course.dart';
 
 void main() {
   runApp(const MyApp());
@@ -13,61 +16,24 @@ class MyApp extends StatelessWidget {
   @override
   Widget build(BuildContext context) {
     return MaterialApp(
+      title: 'Champify',
       debugShowCheckedModeBanner: false,
-      title: 'Course App',
       theme: ThemeData(
+        textTheme: GoogleFonts.poppinsTextTheme(),
         colorScheme: ColorScheme.fromSeed(seedColor: Colors.brown),
         useMaterial3: true,
-
-        // 2. TAMBAHKAN BARIS AJAIB INI:
-        // Ini bikin semua teks di aplikasi otomatis jadi Poppins
-        textTheme: GoogleFonts.poppinsTextTheme(),
       ),
-      home: const CoursePage(),
-    );
-  }
-}
 
-// 3. Ini Halaman Depan (Landing Page)
-class MenuUtama extends StatelessWidget {
-  const MenuUtama({super.key});
+      // Halaman pertama yang muncul
+      home: const WelcomePage(),
 
-  @override
-  Widget build(BuildContext context) {
-    return Scaffold(
-      appBar: AppBar(title: const Text("Aplikasi Belajar")),
-      body: Center(
-        child: Column(
-          mainAxisAlignment: MainAxisAlignment.center,
-          children: [
-            const Text(
-              "Selamat Datang!",
-              style: TextStyle(fontSize: 24, fontWeight: FontWeight.bold),
-            ),
-            const SizedBox(height: 20),
-
-            // 4. Ini Tombol untuk pindah halaman
-            ElevatedButton(
-              style: ElevatedButton.styleFrom(
-                backgroundColor: Colors.brown,
-                foregroundColor: Colors.white,
-                padding: const EdgeInsets.symmetric(
-                  horizontal: 30,
-                  vertical: 15,
-                ),
-              ),
-              onPressed: () {
-                // LOGIKA PINDAH HALAMAN (ROUTING)
-                Navigator.push(
-                  context,
-                  MaterialPageRoute(builder: (context) => const CoursePage()),
-                );
-              },
-              child: const Text("Buka Course Page"),
-            ),
-          ],
-        ),
-      ),
+      // Routing
+      routes: {
+        '/signup': (context) => const SignUpFormPage(),
+        '/login': (context) => const LoginPage(),
+        '/mentoring': (context) => const ZoomMeetingScreen(),
+        '/courses': (context) => const CoursePage(),
+      },
     );
   }
 }
